69c314d1429507118d15f47b1e34049bdeaaffca
Shourya Rai <shouryarai607@gmail.com>
2026-01-14 14:27:23 +0530

    Package

---
+++ Amit.java
@@ -0,0 +1,13 @@
+package Package2;
+import Package1.*;
+
+class Amit
+{
+public static void main(String[] args)
+{
+ Sumit s1=new Sumit();
+       s1.set_data(5,6);
+       s1.display();
+
+}
+}
---
+++ Sumit.java
@@ -0,0 +1,17 @@
+package Package1;
+
+public class Sumit
+{
+private int x;
+private int y;
+public void set_data(int p,int q)
+{
+x=p;
+y=q;
+}
+public void display()
+{
+System.out.println(x);
+System.out.println(y);
+}
+}
