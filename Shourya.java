d1971a69e7c659a48c8755e43b0866a979e995cd
Shourya Rai <shouryarai607@gmail.com>
2026-01-15 22:39:09 +0530

    Package

---
+++ Shourya.java
@@ -0,0 +1,17 @@
+package Shourya;
+
+import Vishu.*;
+import Ved.*;
+
+class Shourya
+{
+public static void main(String[] args)
+{
+Vishu v1= new Vishu();
+      v1.Khitola();
+      v1.Bhopal();
+      v1.Exam();
+Ved v2=new Ved();
+    v2.Chhatisgarh();
+}
+}
