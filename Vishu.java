893feb57aa41b364c54f21cb11d9b3cc2adfcee2
Shourya Rai <shouryarai607@gmail.com>
2026-01-15 22:33:36 +0530

    Package

---
+++ Vishu.java
@@ -0,0 +1,17 @@
+package Vishu;
+
+public class Vishu
+{
+public void Khitola()
+{
+System.out.println("Snap");
+}
+public void Bhopal()
+{
+System.out.println("College");
+}
+public void Exam()
+{
+System.out.println("Exam m aage");
+}
+}
