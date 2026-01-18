cd9172b1aa8caf665c35f0752bcd246d04c06da2
Shourya Rai <shouryarai607@gmail.com>
2026-01-18 22:45:59 +0530

    super keyword

---
+++ Super_keyword.java
@@ -0,0 +1,37 @@
+class Example
+{
+int x;
+int y;
+void f1(int x,int y)
+{
+this.x=x;
+this.y=y;
+}
+void display()
+{
+System.out.println(x);
+System.out.println(y);
+
+}
+}
+class Example2 extends Example
+{
+void f3(int x,int y)
+{
+super.y=y;
+super.x=x;
+}
+}
+
+class Super_keyword
+{
+public static void main(String []args)
+{
+Example e1=new Example();
+       // e1.f1(6,9);
+       // e1.f2();
+Example2 e2=new Example2();
+         e2.f3(4,9);
+         e2.display();
+}
+}
