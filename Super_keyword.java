b025ec784220da763bdfc763fdd2047abe315e1b
Shourya Rai <shouryarai607@gmail.com>
2026-01-21 21:55:44 +0530

    comment

--- Super_keyword.java
+++ Super_keyword.java
@@ -7,13 +7,8 @@ void f1(int x,int y)
 this.x=x;
 this.y=y;
 }
-void display()
-{
-System.out.println(x);
-System.out.println(y);
+}
 
-}
-}
 class Example2 extends Example
 {
 void f3(int x,int y)
@@ -21,17 +16,22 @@ void f3(int x,int y)
 super.y=y;
 super.x=x;
 }
+void display()
+{
+System.out.println(super.x);
+System.out.println(super.y);
+}
 }
 
 class Super_keyword
 {
 public static void main(String []args)
 {
-Example e1=new Example();
+//Example e1=new Example();
        // e1.f1(6,9);
        // e1.f2();
 Example2 e2=new Example2();
-         e2.f3(4,9);
-         e2.display();
+       e2.f3(4,9);
+       e2.display();
 }
 }
