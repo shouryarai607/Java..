44589c6463ae00bb64faeac45d0522c5acbcfe5d
Shourya Rai <shouryarai607@gmail.com>
2026-02-15 23:58:55 +0530

    simple sum

---
+++ simplesum.java
@@ -0,0 +1,13 @@
+public class SimpleSum {
+    public static void main(String[] args) {
+        // Define two numbers
+        int firstNum = 10;
+        int secondNum = 20;
+
+        // Calculate the sum
+        int sum = firstNum + secondNum;
+
+        // Print the result to the screen
+        System.out.println("The total is: " + sum);
+    }
+}
