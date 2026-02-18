40ec20bc4f82e393dffa6c788b5498aa6b39d83d
Shourya Rai <shouryarai607@gmail.com>
2026-02-18 23:10:41 +0530

    correct

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -1,6 +1,5 @@
 public class MyFirstProgram {
-    public static void main(String[] args) {
-        // This line prints the text to the console
+    public static void main(String[] args) 
         System.out.println("Hello, World!");
     }
 }
