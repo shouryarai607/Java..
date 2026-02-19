26f3469e394be012b01a76f1d1894dda0a83979f
Shourya Rai <shouryarai607@gmail.com>
2026-02-19 23:47:56 +0530

    New

---
+++ Greeting.java
@@ -0,0 +1,4 @@
+import java.util.Scanner; // Import the Scanner tool to read input
+
+public class Greeting {
+    public static void main(String[] args) {
--- simplesum.java
+++ simplesum.java
@@ -1,13 +1,11 @@
 public class SimpleSum {
     public static void main(String[] args) {
-        // Define two numbers
         int firstNum = 10;
         int secondNum = 20;
 
-        // Calculate the sum
         int sum = firstNum + secondNum;
 
-        // Print the result to the screen
+        SimpleSum
         System.out.println("The total is: " + sum);
     }
 }
