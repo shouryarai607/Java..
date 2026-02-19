1dd58be4cd446a21ec96923881df381ad03db72e
Shourya Rai <shouryarai607@gmail.com>
2026-02-19 23:48:49 +0530

    Update

--- Greeting.java
+++ Greeting.java
@@ -2,3 +2,6 @@ import java.util.Scanner; // Import the Scanner tool to read input
 
 public class Greeting {
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
