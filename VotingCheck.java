0f0d1f7ba201e6b55221024664b8fe6132e620a1
Shourya Rai <shouryarai607@gmail.com>
2026-02-20 22:58:39 +0530

    New

---
+++ VotingCheck.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class VotingCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
