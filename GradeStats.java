e2a16a93b857b95bc02700294c2c1214580b6825
Shourya Rai <shouryarai607@gmail.com>
2026-02-21 23:55:13 +0530

    update

---
+++ GradeStats.java
@@ -0,0 +1,7 @@
+public class GradeStats {
+    public static void main(String[] args) {
+        // An array of double-precision numbers
+        double[] grades = {85.5, 92.0, 78.5, 99.0, 88.0};
+        
+        double sum = 0;
+        double highest = grades[0];
