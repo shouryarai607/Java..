fd81dcfb4ba9f5ab3f9f05addca36ada6947f4f7
Shourya Rai <shouryarai607@gmail.com>
2026-02-23 22:53:37 +0530

    update

--- QuickStart.java
+++ QuickStart.java
@@ -4,3 +4,7 @@ public class QuickStart {
     public static void main(String[] args) { {
     
 }
+var scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        Object input = scanner.nextLine();
