1665c23043e6c5c6cff1e462a0d086d7d453acdf
Shourya Rai <shouryarai607@gmail.com>
2026-02-23 22:54:39 +0530

    update

--- QuickStart.java
+++ QuickStart.java
@@ -8,3 +8,11 @@ var scanner = new Scanner(System.in);
 
         System.out.print("Enter your name: ");
         Object input = scanner.nextLine();
+        if (input instanceof String s && !s.isBlank()) {
+            System.out.println("Hello, " + s.toUpperCase() + "!");
+            System.out.println("Your name has " + s.length() + " letters.");
+        } else {
+            System.out.println("That doesn't look like a valid name.");
+        }
+    }
+}
