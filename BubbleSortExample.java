0478962d8da205c4e4f8b285a83f0cd801762fd3
Shourya Rai <shouryarai607@gmail.com>
2026-02-24 23:30:47 +0530

    update

--- BubbleSortExample.java
+++ BubbleSortExample.java
@@ -16,3 +16,9 @@ public class BubbleSortExample {
         System.out.println("\nAfter Sorting:");
         printArray(numbers);
     }
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
