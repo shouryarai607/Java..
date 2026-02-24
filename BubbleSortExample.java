b51322d1f20393ff123b48155978b11eab959935
Shourya Rai <shouryarai607@gmail.com>
2026-02-24 23:29:18 +0530

    update

--- BubbleSortExample.java
+++ BubbleSortExample.java
@@ -4,3 +4,12 @@ public class BubbleSortExample {
 
         System.out.println("Before Sorting:");
         printArray(numbers);
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - 1 - i; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
